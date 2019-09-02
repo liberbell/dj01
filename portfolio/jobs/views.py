65db6b9f17e82345657e62be1432ad56dcf65e10
Liberbell <liberbell@gmail.com>
2019-09-02 15:24:39 +0900

    views3

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -10,4 +10,5 @@ def sam(request):
     return render(request, 'jobs/sam.html')
 
 def detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
     return render(request, 'jobs/home.html')
